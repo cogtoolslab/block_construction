8d17d1404fc6a231162ddfabeb54b2142e585b9c
Will McCarthy <mccarthywill@live.co.uk>
2019-10-10 09:55:27 -0700

    added pilot folder

---
+++ experiments/pilot/scripts/boundary.js
@@ -0,0 +1,37 @@
+// Wrapper for Bodies.rectangle class to create boundaries/ walls. 
+// Also includes a show function which draws the box using P5.
+
+function Boundary(x, y, w, h){
+    
+    var options = {
+        isStatic: true, // static i.e. not affected by gravity
+        friction: 0.9,
+        frictionStatic: 2
+        //slop: 0,
+        //restitution: 0
+    }
+    
+    this.w = w;
+    this.h = h;
+
+    this.body = Bodies.rectangle(x,y,w,h, options);
+    World.add(engine.world, this.body); 
+
+    // Display the boundary on the screen
+    this.show = function() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x/sF, pos.y/sF);
+        rectMode(CENTER);
+        rotate(angle);
+        stroke(200);
+        fill(20);
+        rect(0,0,this.w/sF,this.h/sF);
+        pop();
+
+    }
+
+}
