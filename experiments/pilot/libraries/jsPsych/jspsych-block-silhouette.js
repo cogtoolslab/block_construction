e40590861fa7f067964bca0737589cdddb18060d
Judy Fan <judithfan@gmail.com>
2019-10-22 12:32:22 -0700

    comment out most things in jspsych-block-silhouette plugin

--- experiments/pilot/libraries/jsPsych/jspsych-block-silhouette.js
+++ experiments/pilot/libraries/jsPsych/jspsych-block-silhouette.js
@@ -27,7 +27,7 @@ jsPsych.plugins["block-silhouette"] = (function() {
     name: 'block-silhouette',
     description: '',
     parameters: {
-      stimulus: {
+      target: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Block Collection (JSON String)',
         default: undefined,
@@ -81,73 +81,72 @@ jsPsych.plugins["block-silhouette"] = (function() {
 
   plugin.trial = function(display_element, trial) {
 
-    if(typeof trial.stimulus === 'undefined'){
-      console.error('Required parameter "stimulus" missing in block-silhouette');
+    if(typeof trial.target === 'undefined'){
+      console.error('Required parameter "target" missing in block-silhouette');
     }
 
     // wrapper function to show everything, call this when you've waited what you
     // reckon is long enough for the data to come back from the db
     function show_display() {
 
-      //display buttons
-      var buttons = [];
-      if (Array.isArray(trial.button_html)) {
-        if (trial.button_html.length == trial.choices.length) {
-          buttons = trial.button_html;
-        } else {
-          console.error('Error in block-silhouette plugin. The length of the button_html array does not equal the length of the choices array');
-        }
-      } else {
-        for (var i = 0; i < trial.choices.length; i++) {
-          buttons.push(trial.button_html);
-        }
-      }
+      // //display buttons
+      // var buttons = [];
+      // if (Array.isArray(trial.button_html)) {
+      //   if (trial.button_html.length == trial.choices.length) {
+      //     buttons = trial.button_html;
+      //   } else {
+      //     console.error('Error in block-silhouette plugin. The length of the button_html array does not equal the length of the choices array');
+      //   }
+      // } else {
+      //   for (var i = 0; i < trial.choices.length; i++) {
+      //     buttons.push(trial.button_html);
+      //   }
+      // }
 
       //show prompt if there is one
       if (trial.prompt !== null) {
         var html = '<div id="prompt">' +trial.prompt + '</div>';
       }
 
-      html += '<div class="progress"><div id="progress-bar"></div></div>'
+      // html += '<div class="progress"><div id="progress-bar"></div></div>'
 
-      // display sketch (image)
-      html += '<div style="margin-top:20px"><img id="jspsych-block-silhouette-sketch" src="'+ trial.sketch_url +'"></img></div>';
+      // // display sketch (image)
+      // html += '<div style="margin-top:20px"><img id="jspsych-block-silhouette-sketch" src="'+ trial.sketch_url +'"></img></div>';
 
-      html += '<div id="jspsych-block-silhouette-btngroup">';
+      // html += '<div id="jspsych-block-silhouette-btngroup">';
 
-      // embed images inside the response button divs
-      for (var i = 0; i < trial.choices.length; i++) {
-        var str = buttons[i].replace(/%imageURL%/g, trial.choices[i]);
-        var object_id = trial.choices[i].split('/').slice(-1)[0].split('.')[0]; // splice to extract only shapenetID and target_status
-        html += '<div class="jspsych-block-silhouette-button" style="display: inline-block; margin : 0" id="jspsych-block-silhouette-button-' + i +'" data-choice="'+object_id+'">'+str+'</div>'; //'+trial.margin_horizontal+' '+trial.margin_vertical+'"
-      }
+      // // embed images inside the response button divs
+      // for (var i = 0; i < trial.choices.length; i++) {
+      //   var str = buttons[i].replace(/%imageURL%/g, trial.choices[i]);
+      //   var object_id = trial.choices[i].split('/').slice(-1)[0].split('.')[0]; // splice to extract only shapenetID and target_status
+      //   html += '<div class="jspsych-block-silhouette-button" style="display: inline-block; margin : 0" id="jspsych-block-silhouette-button-' + i +'" data-choice="'+object_id+'">'+str+'</div>'; //'+trial.margin_horizontal+' '+trial.margin_vertical+'"
+      // }
 
-      html += '</div>';
+      // html += '</div>';
       
-      // display score earned so far
-      html += '<div id="score"> <p2> bonus earned: ' + parseFloat(score).toFixed(3) + '</p2></div>'
-      html += '<div id="trial-counter"> <p2> trial ' + (parseInt(trial.trialNum)+parseInt(1)).toString() + ' of ' + trial.num_trials + '</p2></div>'
+      // // display score earned so far
+      // html += '<div id="score"> <p2> bonus earned: ' + parseFloat(score).toFixed(3) + '</p2></div>'
+      // html += '<div id="trial-counter"> <p2> trial ' + (parseInt(trial.trialNum)+parseInt(1)).toString() + ' of ' + trial.num_trials + '</p2></div>'
 
       // introduce occluder to make the inter-trial transitions less jarring
       html += '<div id="occluder"> </div>'
 
-      // display helpful info during debugging
-      if (trial.dev_mode==true) {
-        html += '<div id="repetition"> <p> repetition: ' + trial.repetition + '</p></div>'
-        html += '<div id="condition"> <p> condition: ' + trial.condition + '</p></div>'
-      }
+      // // display helpful info during debugging
+      // if (trial.dev_mode==true) {
+      //   html += '<div id="repetition"> <p> repetition: ' + trial.repetition + '</p></div>'
+      //   html += '<div id="condition"> <p> condition: ' + trial.condition + '</p></div>'
+      // }
 
       // actually assign html to display_element.innerHTML
       display_element.innerHTML = html;
 
-      // add click event listener to the image response buttons
-      for (var i = 0; i < trial.choices.length; i++) {
-        $('#jspsych-block-silhouette-button-' + i).on('click', function(e) {
-          var choice = e.currentTarget.getAttribute('data-choice'); // don't use dataset for jsdom compatibility
-          after_response(choice);
-        });      
-      }      
-
+      // // add click event listener to the image response buttons
+      // for (var i = 0; i < trial.choices.length; i++) {
+      //   $('#jspsych-block-silhouette-button-' + i).on('click', function(e) {
+      //     var choice = e.currentTarget.getAttribute('data-choice'); // don't use dataset for jsdom compatibility
+      //     after_response(choice);
+      //   });      
+      // }      
 
     }
 
@@ -162,17 +161,17 @@ jsPsych.plugins["block-silhouette"] = (function() {
     var progressBar = $('#progress-bar');
     var time_bonus = 0;
     
-    progressBar.show();
-    var widthPct = 100 // starts at 105% b/c of the 1000ms delay above before occluder disappears
-    var seconds_passed = 0;
-    var interval = setInterval(function(){
-      seconds_passed += 1;
-      widthPct -= pct_per_sec; // goes down by 5% each second
-      progressBar.animate({ width: widthPct + '%' }, 1000, "linear");
-      if (widthPct <= 0) {
-        clearInterval(interval);
-      }
-    }, 1000);
+    // progressBar.show();
+    // var widthPct = 100 // starts at 105% b/c of the 1000ms delay above before occluder disappears
+    // var seconds_passed = 0;
+    // var interval = setInterval(function(){
+    //   seconds_passed += 1;
+    //   widthPct -= pct_per_sec; // goes down by 5% each second
+    //   progressBar.animate({ width: widthPct + '%' }, 1000, "linear");
+    //   if (widthPct <= 0) {
+    //     clearInterval(interval);
+    //   }
+    // }, 1000);
 
     // store response
     var response = {
@@ -185,27 +184,23 @@ jsPsych.plugins["block-silhouette"] = (function() {
     function after_response(choice) {
       // console.log('after response function called');
       
-      // End timer
-      clearInterval(interval);
-      progressBar.stop();
+      // // End timer
+      // clearInterval(interval);
+      // progressBar.stop();
       
       // measure rt
       var end_time = Date.now();
       var rt = end_time - start_time;
-      response.button = choice;
       response.rt = rt;
-      time_bonus = (max_time_bonus - seconds_passed * decrement_per_sec); //time_passed in units of seconds
-      // console.log('time passed: ', seconds_passed);
-      // console.log("response time bonus: " + time_bonus)
 
-      // after a valid response, the sketch will have the CSS class 'responded'
-      // which can be used to provide visual feedback that a response was recorded
-      display_element.querySelector('#jspsych-block-silhouette-sketch').className += ' responded';
+      // // after a valid response, the sketch will have the CSS class 'responded'
+      // // which can be used to provide visual feedback that a response was recorded
+      // display_element.querySelector('#jspsych-block-silhouette-sketch').className += ' responded';
 
-      // disable all the buttons after a response
-      for (var i = 0; i < trial.choices.length; i++) {
-        $('#jspsych-block-silhouette-button-' + i).off('click');
-      }      
+      // // disable all the buttons after a response
+      // for (var i = 0; i < trial.choices.length; i++) {
+      //   $('#jspsych-block-silhouette-button-' + i).off('click');
+      // }      
 
       if (trial.response_ends_trial) {
         end_trial();  
@@ -221,41 +216,40 @@ jsPsych.plugins["block-silhouette"] = (function() {
       // get info from mturk
       var turkInfo = jsPsych.turk.turkInfo();
 
-      // prettify choices list
-      var prettyChoices = new Array;
-      _.forEach(trial.choices, function(x) {
-        prettyChoices.push(x.split('/').slice(-1)[0].split('.')[0]);
-      });
+      // // prettify choices list
+      // var prettyChoices = new Array;
+      // _.forEach(trial.choices, function(x) {
+      //   prettyChoices.push(x.split('/').slice(-1)[0].split('.')[0]);
+      // });
 
-      // check if response matches target, i.e., whether response is correct
-      var trial_correct;
-      if (response.button == trial.target.shapenetid) {
-        trial_correct = 1;
-        increment = accuracy_bonus + parseFloat(time_bonus);
-        score+= parseFloat(increment); // increment accuracy bonus and time bonus
-        numCorrect += 1; // increment num correct by one
-      } else {
-        trial_correct = 0;
-      }
+      // // check if response matches target, i.e., whether response is correct
+      // var trial_correct;
+      // if (response.button == trial.target.shapenetid) {
+      //   trial_correct = 1;
+      //   increment = accuracy_bonus + parseFloat(time_bonus);
+      //   score+= parseFloat(increment); // increment accuracy bonus and time bonus
+      //   numCorrect += 1; // increment num correct by one
+      // } else {
+      //   trial_correct = 0;
+      // }
 
       // gather the data to store for the trial
       if (trial.dev_mode == true) {
         console.log(trial.choices);
       }
-      var trial_data = _.extend(_.omit(trial, 'on_finish', 'choices', 'outcome'), {
-          dbname: '3dObjects',
-          colname: 'graphical_conventions_recog',
-          ordering: _.zipObject(prettyChoices, _.range(prettyChoices.length)),
-          rt: response.rt,
-          correct: trial_correct,
-          numCorrectSoFar: numCorrect,
-          original_correct: trial.outcome,
-          stim_mongo_id: trial._id,
-          response: response.button,
-          score: score,
-          workerId: turkInfo.workerId,
-          hitID: turkInfo.hitId,
-          aID: turkInfo.assignmentId,
+      var trial_data = _.extend(_.omit(trial, 'on_finish'), {
+          dbname: 'block_construction',
+          colname: 'silhouette',
+          // rt: response.rt,
+          // correct: trial_correct,
+          // numCorrectSoFar: numCorrect,
+          // original_correct: trial.outcome,
+          // stim_mongo_id: trial._id,
+          // response: response.button,
+          // score: score,
+          // workerId: turkInfo.workerId,
+          // hitID: turkInfo.hitId,
+          // aID: turkInfo.assignmentId,
           timestamp: Date.now()
       });
 
@@ -264,30 +258,30 @@ jsPsych.plugins["block-silhouette"] = (function() {
         console.log('correct?  ', trial_correct);
       }
 
-    // get location index of target
-    target_index = _.indexOf(prettyChoices, trial.target.shapenetid);
-    response_index = _.indexOf(prettyChoices, response.button);
-    if (trial.dev_mode == true) {
-      console.log('target_index: ', target_index); 
-      console.log('response_index: ', response_index); 
-    }
+    // // get location index of target
+    // target_index = _.indexOf(prettyChoices, trial.target.shapenetid);
+    // response_index = _.indexOf(prettyChoices, response.button);
+    // if (trial.dev_mode == true) {
+    //   console.log('target_index: ', target_index); 
+    //   console.log('response_index: ', response_index); 
+    // }
 
-    // show feedback
-    if (trial_correct==true) {
-     // show feedback by drawing GREEN box around TARGET if selected CORRECTLY    
-      display_element.querySelector('#jspsych-block-silhouette-button-' + target_index).style.border = "8px solid #66B03B"
-      // also bold/enlarge the score in bottom left corner 
-      display_element.querySelector('#score p2').innerHTML = 'bonus earned: ' + parseFloat(score).toFixed(3);
-      display_element.querySelector('#score p2').style.fontWeight = 'bold';
-    } else {
-     // draw RED box around INCORRECT response and BLACK box around TARGET
-     display_element.querySelector('#jspsych-block-silhouette-button-' + target_index).style.border = "8px solid #282828"      
-     display_element.querySelector('#jspsych-block-silhouette-button-' + response_index).style.border = "8px solid #D02B16"      
-    }
+    // // show feedback
+    // if (trial_correct==true) {
+    //  // show feedback by drawing GREEN box around TARGET if selected CORRECTLY    
+    //   display_element.querySelector('#jspsych-block-silhouette-button-' + target_index).style.border = "8px solid #66B03B"
+    //   // also bold/enlarge the score in bottom left corner 
+    //   display_element.querySelector('#score p2').innerHTML = 'bonus earned: ' + parseFloat(score).toFixed(3);
+    //   display_element.querySelector('#score p2').style.fontWeight = 'bold';
+    // } else {
+    //  // draw RED box around INCORRECT response and BLACK box around TARGET
+    //  display_element.querySelector('#jspsych-block-silhouette-button-' + target_index).style.border = "8px solid #282828"      
+    //  display_element.querySelector('#jspsych-block-silhouette-button-' + response_index).style.border = "8px solid #D02B16"      
+    // }
 
-    // wait to screen and moving onto next trial until you show feedback
-    jsPsych.pluginAPI.setTimeout(function() {
-                      clear_display_move_on(trial_data);},2000);      
+    // // wait to screen and moving onto next trial until you show feedback
+    // jsPsych.pluginAPI.setTimeout(function() {
+    //                   clear_display_move_on(trial_data);},2000);      
 
     };
 
@@ -302,14 +296,14 @@ jsPsych.plugins["block-silhouette"] = (function() {
     };
 
     // hide image if timing is set
-    if (trial.sketch_duration !== null) {
+    if (trial.explore_duration !== null) {
       jsPsych.pluginAPI.setTimeout(function() {
         display_element.querySelector('#jspsych-block-silhouette-sketch').style.visibility = 'hidden';
       }, trial.sketch_duration);
     }
 
     // end trial if time limit is set
-    if (trial.trial_duration !== null) {
+    if (trial.build_duration !== null) {
       jsPsych.pluginAPI.setTimeout(function() {
         end_trial();
       }, trial.trial_duration);
