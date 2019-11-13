262e25741b28aabc5b7ee3036d1a57df218c3d85
Judy Fan <judithfan@gmail.com>
2019-11-13 11:18:34 -0800

    add bit about practice trial

--- experiments/pilot/js/setup.js
+++ experiments/pilot/js/setup.js
@@ -29,8 +29,8 @@ var consentHTML = {
 var instructionsHTML = {
   'str1' : "<p> Here's how the game will work: On each trial, you will see a block structure appear on the left. Your goal is to use the blocks on the right to reconstruct it using as few blocks as possible. <div><img src='images/task_display.png' id='example_screen'></div>",
   'str2' : '<p> There are 8 trials in this HIT. For really good reconstructions, you will receive a <b> bonus</b> of X.X cents.</p>',
-  'str3' : "<p>If you encounter a problem or error, send us an email (sketchloop@gmail.com) and we will make sure you're compensated for your time! Please pay attention and do your best! </p><p> Note: We recommend using Chrome. We have not tested this HIT in other browsers.</p>",  
-  'str4' : "<p> Once you are finished, the HIT will be automatically submitted for approval. Please know that you can only perform this HIT one time. Before we begin, please complete a brief questionnaire to show you understand how this HIT works.</p>"
+  'str3' : "<p>If you encounter a problem or error, send the researchers an email (sketchloop@gmail.com) to arrange for prorated compensation. Please pay attention and do your best! </p><p> Note: We recommend using Chrome. We have not tested this HIT in other browsers.</p>",  
+  'str4' : "<p> Once you are finished, the HIT will be automatically submitted for approval. Please know that you can only perform this HIT one time. Before we begin, you will get some practice with the reconstruction task. On this practice trial, you will be shown the exact locations to place each block. Please do your best on this practice trial to ensure that you have the opportunity to proceed and participate in the experiment.</p>"
 };
 
 var welcomeTrial = {
