ce4b19515f2afe250365feb015fdd8077609e1c6
Will McCarthy <wmccarthy@ucsd.edu>
2020-01-28 22:41:23 -0800

    Double checking apparent pre-difference, add R script

---
+++ analysis/cogsci_2020.R
@@ -0,0 +1,46 @@
+setwd("~/GitHub/block_construction/analysis")
+
+library(broom)
+library(tidyverse)
+library(knitr)
+library(ggplot2)
+library(broom)
+library(lme4)
+library(lmerTest)
+knitr::opts_chunk$set(echo = TRUE)
+
+# import data
+df.full = read.csv('results/csv/block_silhouette_Exp2Pilot3.csv')
+
+df <- df.full %>% 
+  select(rawF1DiscreteScore,phase,condition,trialNum,targetName,gameID)
+
+phase_condition = lm(data = df, rawF1DiscreteScore~phase)
+phase_condition
+anova(phase_condition)
+
+#m = lmer(data = df, rawF1DiscreteScore ~ phase * condition + (1 + phase*condition | gameID) + (1 + phase*condition | targetName))
+
+
+## Test for difference in conditions in pre phase 
+df.pre <- df %>% 
+  filter(phase=='pre')
+
+pre_means <- aggregate(rawF1DiscreteScore~gameID+condition, data=df.pre, mean) %>% 
+  pivot_wider(gameID,names_from=condition,values_from=rawF1DiscreteScore)
+
+t.test(pre_means$control, pre_means$repeated, paired=TRUE)
+
+
+df.post<- df %>% 
+  filter(phase=='post')
+
+post_means <- aggregate(rawF1DiscreteScore~gameID+condition, data=df.post, mean) %>% 
+  pivot_wider(gameID,names_from=condition,values_from=rawF1DiscreteScore)
+
+t.test(post_means$control, post_means$repeated, paired=TRUE)
+
+pre_means %>% 
+  mutate(diff = .$control-.$repeated) %>% 
+  t.test(.$diff, data=., conf.level=.95)
+
