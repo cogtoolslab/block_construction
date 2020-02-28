1b3920329760bd23cc244f18300ca3019eee22dd
wpmccarthy <mccarthywill@live.co.uk>
2020-02-27 16:08:18 -0800

    more function coding

--- analysis/will/rda.py
+++ analysis/will/rda.py
@@ -1,16 +1,47 @@
 def rda_df(df, 
            cols, 
-           row_dim = cols[0], 
+           row_col = 0, 
 #           col_dim = cols[0], 
-           vec_dim = cols[1], 
-           layer_dim = cols[2], 
-           small_multiple_dim = cols[3], 
-           layer_dim = None, 
-           distance_measure = (lambda a,b: distance.euclidean(a,b))
-          ):
-   
-    x = df.unique(df)
-    n_x = df.nunique(df)
+           value_col = 1, 
+           layer_col = None, 
+           small_multiple_col = None, 
+           distance_measure = distance.euclidean):
+    '''
+    Creates RDA matrices for a dataframe and specified columns
+    '''
+
+    
+    row_vals = np.sort(df[row_col].unique())
+    
+    n_layers = 0
+    n_small_multiples = 0
+    
+    if not (layer_col == None):
+        layer_values = np.sort(df[layer_col].unique())
+        n_layers = df[layer_col].nunique()
+        
+        
+    if not (small_multiple_col == None):
+        small_multiple_values = np.sort(df[small_multiple_col].unique())
+        n_small_multiples = df[small_multiple_col].nunique()
+    
+    n_reps = df['repetition'].nunique()
+    vector_size = len(df.value_col[0]) #1 or length of vector
+
+    val_mat = np.zeros((n_targets, n_ppts, max_actions, n_reps, n_grid_squares))
+
+    for i_t, target in enumerate(targets):
+        for i_p, ppt in enumerate(ppts):
+            for action in range(0, max_actions):
+                for rep in range(0, n_reps):
+                    world_df = dfic[(dfic.targetName==target) &\
+                             (dfic.gameID==ppt) &\
+                             (dfic.blockNum==action) &\
+                             (dfic.repetition==rep)]['flatDiscreteWorld']
+                    if (not world_df.empty):
+                        val_mat[i_t,i_p,action,rep,:] = world_df.iloc[0]
+
+
     
 
 def rda_mat(mat, 
@@ -43,16 +74,34 @@ def rda_mat(mat,
         vec_dim = 0
         while ((vec_dim < len(dims)) & not(vec_dim in [row_dim,layer_dim,small_multiple_dim])):
             vec_dim += 1
-        
+    
+    # set dimensions of distance tensor
     n_rows = dims[row_dim]
+    n_cols = n_rows
 
-    all_dists = np.zeros((n_targets, n_ppts, max_actions, n_rows, n_cols))
+    # account for layers/ small multiples
+    if layer_dim == None:
+        n_layers = 1
+    else:
+        n_layers = dims[layer_dim]
+    
+    if small_multiple_dim == None:
+        n_small_multiples = 1
+    else:
+        n_small_multiples = dims[small_multiple_dim]
+    
+    all_dists = np.zeros((n_rows, n_cols, n_layers, n_small_multiples))
+    
+    index_array = [0,0,0,0,0]
+    index_array[row_dim] = row
+    index_array[col_dim] = col
+    index_array[layer_dim] = layer
+    index_array[small_multiple_dim] = n_small_multiples
 
-    for t in range(0, n_targets):
-        for p in range(0, n_ppts):
-            for a in range(0, max_actions):
-                for rep_a in range (0, n_rows): # row dim
-                    for rep_b in range (0, n_cols): # col dim
-                        world_a = all_worlds[t,p,a,rep_a,:]
-                        world_b = all_worlds[t,p,a,rep_b,:]
-                        all_dists[t,p,a,rep_a,rep_b] = scoring.get_jaccard(world_a,world_b)
+    for multiple in range(0, n_small_multiples):
+        for layer in range(0, n_layers):
+            for row in range(0, n_rows): # row dim
+                for col in range(0, n_cols): # col dim
+                    vec_row = mat[row,layer,multiple,:]
+                    vec_col = mat[col,layer, multiple,:]
+                    all_dists[row, col, layer, multiple] = distance_measure(vec_row,vec_col)
