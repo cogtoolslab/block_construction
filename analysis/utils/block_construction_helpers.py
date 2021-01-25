9e68ee6debad9df062623ab51bde378dd456c3ad
wpmccarthy <mccarthywill@live.co.uk>
2021-01-24 19:23:21 -0800

    explore height and area

--- analysis/utils/block_construction_helpers.py
+++ analysis/utils/block_construction_helpers.py
@@ -42,6 +42,19 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
+### target bit maps ###
+
+targetMaps = {}
+
+with open('../results/silhouette/csv/targetMaps.txt') as json_file:
+    targetMaps = json.load(json_file)
+    
+
+def get_target_cropped(target):
+    return (np.logical_not(np.array(targetMaps[target]))*1)[5:13,0:8]
+
+
+### conversions between arrays and strings, cropping, etc ###
 
 def convert_to_str(flat_world):
     s = [str(i) for i in list(flat_world)] 
@@ -59,13 +72,22 @@ def cropped_chunk_to_string(list_chunk):
 
 def chunk_str_to_cropped_array(chunk_str):
     return np.array([int(x) for x in chunk_str]).reshape((18,13))[5:13,0:8]
-    
-    
-targetMaps = {}
 
-with open('../results/silhouette/csv/targetMaps.txt') as json_file:
-    targetMaps = json.load(json_file)
-    
+def chunk_str_to_full_array(chunk_str):
+    return np.array([int(x) for x in chunk_str]).reshape((18,13))
 
-def get_target_cropped(target):
-    return (np.logical_not(np.array(targetMaps[target]))*1)[5:13,0:8]
+### properties of chunks ###
+
+def chunk_height(chunk, chunk_is_string=True):
+    if chunk_is_string:
+        chunk = chunk_str_to_cropped_array(chunk)
+        
+    height = np.sum(np.dot(np.sum(chunk, axis=0),np.arange(8)))/np.sum(chunk) + 0.5
+    return height
+
+def chunk_area(chunk, chunk_is_string=True):
+    if chunk_is_string:
+        chunk = chunk_str_to_full_array(chunk) #full array, as chunk may extend beyond silhouette
+        
+    area = np.sum(chunk)
+    return area
